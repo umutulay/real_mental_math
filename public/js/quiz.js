cb5f0d9e961d60d3c15eab7a829b042f5587d575
Umutt <umutulay1907@gmail.com>
2025-03-04 02:48:48 -0600

    comment added for multip

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -24,7 +24,7 @@ function generateSubstractionQuestion() {
 
 function generateMultiplicationQuestion() {
     num1 = Math.floor(Math.random() * 101); // Random number between 0 and 100
-    num2 = Math.floor(Math.random() * 11);
+    num2 = Math.floor(Math.random() * 11); // Random number between 0 and 10
     correctAnswer = num1 * num2;
     document.getElementById("question").textContent = `${num1} x ${num2}`;
 }
