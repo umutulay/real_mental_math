ec08b97fcb23183296dd959da7b591222201ca03
Umutt <umutulay1907@gmail.com>
2025-03-06 02:04:28 -0600

    export function ?

--- public/js/operations/addition.js
+++ public/js/operations/addition.js
@@ -1,18 +1,18 @@
-function generateAdditionEasyQuestion() {
+export function generateAdditionEasyQuestion() {
     num1 = Math.floor(Math.random() * 11); // Random number between 0 and 10
     num2 = Math.floor(Math.random() * 11);
     correctAnswer = num1 + num2;
     document.getElementById("question").textContent = `${num1} + ${num2}`;
 }
 
-function generateAdditionMediumQuestion() {
+export function generateAdditionMediumQuestion() {
     num1 = Math.floor(Math.random() * 11); // Random number between 0 and 10
     num2 = Math.floor(Math.random() * 101); // Random number between 0 and 100
     correctAnswer = num1 + num2;
     document.getElementById("question").textContent = `${num1} + ${num2}`;
 }
 
-function generateAdditionHardQuestion() {
+export function generateAdditionHardQuestion() {
     num1 = Math.floor(Math.random() * 101); // Random number between 0 and 100
     num2 = Math.floor(Math.random() * 101);
     correctAnswer = num1 + num2;
