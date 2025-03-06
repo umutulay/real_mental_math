f10c8afe9ac03928b6eecbafd64a4743eb5cb21f
Umutt <umutulay1907@gmail.com>
2025-03-06 02:50:54 -0600

    div

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -77,7 +77,7 @@ function generateMultiplicationHardQuestion() {
 
 function generateDivisionEasyQuestion() {
     num2 = Math.floor(Math.random() * 11) + 1; // Random number between 1 and 10
-    num1 = num2 * (Math.floor(Math.random() * 30) + 1); // Random number between 1 and 30
+    num1 = num2 * (Math.floor(Math.random() * 11) + 1); // Random number between 1 and 10
     correctAnswer = num1 / num2;
     document.getElementById("question").textContent = `${num1} / ${num2}`;
 }
