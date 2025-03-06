f49411fd796e9c7ed034cdbe8fce9a005aa573ec
Umutt <umutulay1907@gmail.com>
2025-03-06 01:53:48 -0600

    added diffi for add

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,8 @@
 function startQuiz() {
     const selectedType = document.getElementById("quiz-type").value;
     localStorage.setItem("quizType", selectedType); // Store the selection
+    const selectedDiffi = document.getElementById("difficulty").value;
+    localStorage.setItem("difficulty", selectedDiffi); // Store the selection    
     window.location.href = "/quiz"; // Redirect to quiz page
 }
 
