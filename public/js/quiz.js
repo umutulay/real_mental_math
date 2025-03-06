d9e6227890e190e75acfdee004dc63a3ad376c4a
Umutt <umutulay1907@gmail.com>
2025-03-06 02:50:24 -0600

    multip diffi

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -76,7 +76,7 @@ function generateMultiplicationHardQuestion() {
 }
 
 function generateDivisionEasyQuestion() {
-    num2 = Math.floor(Math.random() * 101) + 1; // Random number between 1 and 100
+    num2 = Math.floor(Math.random() * 11) + 1; // Random number between 1 and 10
     num1 = num2 * (Math.floor(Math.random() * 30) + 1); // Random number between 1 and 30
     correctAnswer = num1 / num2;
     document.getElementById("question").textContent = `${num1} / ${num2}`;
