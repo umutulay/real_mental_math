e88d38a8c51dab83913acb65fe9bbf9c6c6be088
Umutt <umutulay1907@gmail.com>
2025-03-06 02:13:01 -0600

    modular

--- public/js/operations/multiplication.js
+++ public/js/operations/multiplication.js
@@ -1,6 +1,7 @@
 export function generateMultiplicationQuestion() {
-    num1 = Math.floor(Math.random() * 101); // Random number between 0 and 100
-    num2 = Math.floor(Math.random() * 11); // Random number between 0 and 10
-    correctAnswer = num1 * num2;
+    let num1 = Math.floor(Math.random() * 101); // Random number between 0 and 100
+    let num2 = Math.floor(Math.random() * 11); // Random number between 0 and 10
+    let correctAnswer = num1 * num2;
     document.getElementById("question").textContent = `${num1} x ${num2}`;
+    return correctAnswer;
 }
