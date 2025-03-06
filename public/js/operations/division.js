d10aa7edeabe6dfeaccdb0bb0b1ee89874998954
Umutt <umutulay1907@gmail.com>
2025-03-06 02:09:00 -0600

    modular

--- public/js/operations/division.js
+++ public/js/operations/division.js
@@ -1,4 +1,4 @@
-function generateDivisionQuestion() {
+export function generateDivisionQuestion() {
     num2 = Math.floor(Math.random() * 101) + 1; // Random number between 1 and 100
     num1 = num2 * (Math.floor(Math.random() * 30) + 1); // Random number between 1 and 30
     correctAnswer = num1 / num2;
