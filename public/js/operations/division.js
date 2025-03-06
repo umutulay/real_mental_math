45d00431e915b14acd98711146eba0776e1ec8f3
Umutt <umutulay1907@gmail.com>
2025-03-06 02:40:24 -0600

    module added package.json

--- public/js/operations/division.js
+++
@@ -1,8 +0,0 @@
-export function generateDivisionQuestion() {
-    let num2 = Math.floor(Math.random() * 101) + 1; // Random number between 1 and 100
-    let num1 = num2 * (Math.floor(Math.random() * 30) + 1); // Random number between 1 and 30
-    let correctAnswer = num1 / num2;
-    document.getElementById("question").textContent = `${num1} / ${num2}`;
-    return correctAnswer;
-}
-
--- public/js/operations/multiplication.js
+++
@@ -1,7 +0,0 @@
-export function generateMultiplicationQuestion() {
-    let num1 = Math.floor(Math.random() * 101); // Random number between 0 and 100
-    let num2 = Math.floor(Math.random() * 11); // Random number between 0 and 10
-    let correctAnswer = num1 * num2;
-    document.getElementById("question").textContent = `${num1} x ${num2}`;
-    return correctAnswer;
-}
