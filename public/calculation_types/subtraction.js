5ed3d6431a952f960b4409b4c24a04bcfa5f5bdc
Umutt <umutulay1907@gmail.com>
2025-03-17 19:26:15 -0500

    modular subt and add

--- public/calculation_types/subtraction.js
+++ public/calculation_types/subtraction.js
@@ -1,6 +1,6 @@
 export function generateSubtractionEasyQuestion() {
-    num1 = Math.floor(Math.random() * 10); // Random number between 0 and 9
-    num2 = Math.floor(Math.random() * 10);
+    const num1 = Math.floor(Math.random() * 10); // Random number between 0 and 9
+    const num2 = Math.floor(Math.random() * 10);
     return {
         question: `${num1} - ${num2}`,
         correctAnswer: num1 - num2
@@ -8,8 +8,8 @@ export function generateSubtractionEasyQuestion() {
 }
 
 export function generateSubtractionMediumQuestion() {
-    num1 = Math.floor(Math.random() * 100); // Random number between 0 and 99
-    num2 = Math.floor(Math.random() * 10);
+    const num1 = Math.floor(Math.random() * 100); // Random number between 0 and 99
+    const num2 = Math.floor(Math.random() * 10);
     return {
         question: `${num1} - ${num2}`,
         correctAnswer: num1 - num2
@@ -17,8 +17,8 @@ export function generateSubtractionMediumQuestion() {
 }
 
 export function generateSubtractionHardQuestion() {
-    num1 = Math.floor(Math.random() * 100); // Random number between 0 and 99
-    num2 = Math.floor(Math.random() * 100);
+    const num1 = Math.floor(Math.random() * 100); // Random number between 0 and 99
+    const num2 = Math.floor(Math.random() * 100);
     return {
         question: `${num1} - ${num2}`,
         correctAnswer: num1 - num2
