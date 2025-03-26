7f1b085d971691d1e58436c9efc4a3a715d65dc4
Umutt <umutulay1907@gmail.com>
2025-03-26 00:43:19 -0500

    correct answer not working

--- public/js/division.mjs
+++ public/js/division.mjs
@@ -2,7 +2,7 @@ export function generateDivisionEasyQuestion() {
     const num2 = Math.floor(Math.random() * 10) + 1;    // Random number between 0 and 9
     const num1 = num2 * (Math.floor(Math.random() * 10) + 1);
     const correctAnswer = num1 / num2;
-    document.getElementById("question").textContent = `${num1} /S ${num2}`;
+    document.getElementById("question").textContent = `${num1} / ${num2}`;
     return correctAnswer;
 }
 
@@ -10,7 +10,7 @@ export function generateDivisionMediumQuestion() {
     const num2 = Math.floor(Math.random() * 100) + 1;    // Random number between 0 and 9
     const num1 = num2 * (Math.floor(Math.random() * 30) + 1);
     const correctAnswer = num1 / num2;
-    document.getElementById("question").textContent = `${num1} /S ${num2}`;
+    document.getElementById("question").textContent = `${num1} / ${num2}`;
     return correctAnswer;
 }
 
@@ -18,6 +18,6 @@ export function generateDivisionHardQuestion() {
     const num2 = Math.floor(Math.random() * 100) + 1;    // Random number between 0 and 9
     const num1 = num2 * (Math.floor(Math.random() * 100) + 1);
     const correctAnswer = num1 / num2;
-    document.getElementById("question").textContent = `${num1} /S ${num2}`;
+    document.getElementById("question").textContent = `${num1} / ${num2}`;
     return correctAnswer;
 }
