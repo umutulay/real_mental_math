f678a0a5f90b12243ecc3b1a8a4250f110f6c784
Umutt <umutulay1907@gmail.com>
2025-03-26 00:24:02 -0500

    modular addition

--- public/js/addition.mjs
+++ public/js/addition.mjs
@@ -1,26 +1,23 @@
 export function generateAdditionEasyQuestion() {
     const num1 = Math.floor(Math.random() * 10);    // Random number between 0 and 9
     const num2 = Math.floor(Math.random() * 10);
-    return {
-        question: `${num1} + ${num2}`,
-        correctAnswer: num1 + num2
-    };
+    const correctAnswer = num1 + num2;
+    document.getElementById("question").textContent = `${num1} + ${num2}`;
+    return correctAnswer;
 }
 
 export function generateAdditionMediumQuestion() {
     const num1 = Math.floor(Math.random() * 10);
     const num2 = Math.floor(Math.random() * 100);   // Random number between 0 and 99
-    return {
-        question: `${num1} + ${num2}`,
-        correctAnswer: num1 + num2
-    };
+    const correctAnswer = num1 + num2;
+    document.getElementById("question").textContent = `${num1} + ${num2}`;
+    return correctAnswer;
 }
 
 export function generateAdditionHardQuestion() {
     const num1 = Math.floor(Math.random() * 100); 
     const num2 = Math.floor(Math.random() * 100);
-    return {
-        question: `${num1} + ${num2}`,
-        correctAnswer: num1 + num2
-    };
+    const correctAnswer = num1 + num2;
+    document.getElementById("question").textContent = `${num1} + ${num2}`;
+    return correctAnswer;
 }
